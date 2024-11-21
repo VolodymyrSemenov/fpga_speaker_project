5f0900f4cc294273b73ad4f16f9394af6cd0a0cd
Semenov <semenov@buffalo.edu>
2024-11-21 03:06:26 -0500

    State Machine

--- ultrasonicSpeaker/ultrasonicSpeaker.srcs/sources_1/new/UltrasonicSpeaker.vhd
+++ ultrasonicSpeaker/ultrasonicSpeaker.srcs/sources_1/new/UltrasonicSpeaker.vhd
@@ -38,17 +38,9 @@ signal ready : std_logic;
 signal slow_clock_out: std_logic := '0';
 signal counter: unsigned(26 downto 0) := (others => '0');
 
-signal tone_terminal_count: unsigned(6 downto 0) := (others => '0');
-signal tone_counter: unsigned(6 downto 0) := (others => '0');
-signal half_tone_terminal_count: unsigned(6 downto 0) := (others => '0');
-
-constant COUNT_C5 : unsigned (6 downto 0) := TO_UNSIGNED(92, 7);
-constant COUNT_E5 : unsigned (6 downto 0) := TO_UNSIGNED(73, 7);
-constant COUNT_G5 : unsigned (6 downto 0) := TO_UNSIGNED(61, 7);
-constant COUNT_C6 : unsigned (6 downto 0) := TO_UNSIGNED(46, 7);
-
 type state_type is (IDLE, NOTE1, NOTE2, NOTE3, NOTE4);
 signal state: state_type := IDLE;
+signal something: STD_LOGIC_VECTOR(11 downto 0);
 
 begin
 
@@ -75,13 +67,12 @@ clk_gen : entity work.combined_clock_gen port map (
 );
 
 mclk <= codec_clock;
-half_tone_terminal_count <= '0' & tone_terminal_count(6 downto 1); 
 
 
 --test <= mic_data(22) & mic_data(20) & mic_data(18) & mic_data(16) & mic_data(14) & mic_data(12) & mic_data(10) & mic_data(8) & mic_data(6) & mic_data(4) & mic_data(2) & mic_data(0);
---mic_data_12 <= unsigned(test);
-mic_data_12 <= unsigned(out_data);
-test <= out_data;
+mic_data_12 <= unsigned(test);
+--mic_data_12 <= unsigned(out_data);
+--test <= out_data;
 
 
 pwm_proc : process(pwm_clock)
@@ -94,16 +85,16 @@ begin
             modulated <= '1'; 
         end if;
         
---        sound_counter <= sound_counter + 1;
---        if sound_counter < X"4000" then
---            test <= x"400";
---        elsif sound_counter < X"8000" then 
---            test <= x"800";
---        elsif sound_counter < X"C000" then 
---            test <= x"400";
---        else
---            test <= x"000";
---        end if;
+        sound_counter <= sound_counter + 1;
+        if sound_counter < X"4000" then
+            test <= '0' & something(10 downto 0);
+        elsif sound_counter < X"8000" then 
+            test <= something;
+        elsif sound_counter < X"C000" then 
+            test <= '0' & something(10 downto 0);
+        else
+            test <= x"000";
+        end if;
     end if;
 end process pwm_proc;
 
@@ -121,57 +112,39 @@ begin
     end if;
 end process slow_clock_gen;
 
-tone_counter_proc: process(codec_clock)
-begin 
-    if rising_edge(codec_clock) then
-        --if ready = '1' then 
-            if tone_counter = tone_terminal_count then
-                tone_counter <= (others => '0');
-            else
-                tone_counter <= tone_counter + 1;
-            end if;
-            
-            if tone_counter < half_tone_terminal_count then     
-                out_data <= X"FFF" ; 
-            else 
-                out_data <= (others => '0');
-            end if;
-        --end if; 
-    end if; 
- end process tone_counter_proc;
  
- state_transition: process(codec_clock)
+state_transition: process(codec_clock)
 begin   
      if rising_edge(codec_clock) then
         case state is 
             when IDLE =>  
                 if start = '1' then 
-                    tone_terminal_count <= COUNT_C5;
                     state <= NOTE1;
+                    something <= x"200";
                 end if; 
             
             when NOTE1 =>  
                 if slow_clock_out = '1' then 
-                    tone_terminal_count <= COUNT_E5;
                     state <= NOTE2;
+                    something <= x"600";
                 end if; 
             
             when NOTE2 =>  
                 if slow_clock_out = '1' then 
-                    tone_terminal_count <= COUNT_G5;
                     state <= NOTE3;
+                    something <= x"A00";
                 end if; 
             
             when NOTE3 =>  
                 if slow_clock_out = '1' then 
-                    tone_terminal_count <= COUNT_C6;
                     state <= NOTE4;
+                    something <= x"F00";
                 end if; 
             
             when NOTE4 =>  
                 if slow_clock_out = '1' then 
-                    tone_terminal_count <= (others=>'0');
                     state <= IDLE;
+                    something <= x"800";
                 end if; 
         end case;
     end if;
